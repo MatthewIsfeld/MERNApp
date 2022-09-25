7bb68dcdef1773af4a4a4f8e03b394b318c76b9d
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-09-25 14:29:01 -0400

    Installed mongoose to use mongoDB and set up the connection string to connect to the database

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,21 @@
 require('dotenv').config();
 //import express
 const express = require('express');
+const mongoose = require('mongoose');
 //import our user router
 const userRouter = require('./routes/userRoutes.js');
 
 //create express app
 const app = express();
 
-//listen to a port
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening to port: ${process.env.PORT}`);
-})
+//connect to the database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen to a port, we only want to listen to a port once we are connected to the database
+        app.listen(process.env.PORT, () => {
+            console.log(`The Server is connected to the database and is listening on port: ${process.env.PORT}`);
+        })
+    })
 
 //user the user router to handle all routes related to the user
 app.use('/app/user', userRouter);
