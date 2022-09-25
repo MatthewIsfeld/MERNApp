8d05ca1ac9b7ca5d1729e06384a70494b6d6d998
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-09-25 14:56:15 -0400

    Used bcrypt and validator packages to add validation and password hashing to the user singup process

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,9 +8,19 @@ const loginUser = async (req, res) => {
 }
 
 const signupUser = async (req, res) => {
+    //get the email and password from the user's request
     const {email, password} = req.body;
-    const user = await User.signup(email, password);
-    res.status(200).json({ user });
+
+    //attempt to create a user, in our singup function we throw errors if there is a problem so use a try catch block
+    try {
+        const user = await User.signup(email, password);
+        
+        res.status(200).json({ user });       
+    } catch (error) {
+        //if there is an error send the error back to the user
+        res.status(400).json({error: error.message});   
+    }
+
 }
 
 //export our controller functions so we can use them in our routers
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+//import bcrypt to hash password
+const bcrypt = require('bcrypt');
+//import validator to validate emails and passwords
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -18,7 +22,37 @@ const userSchema = new Schema({
 
 //create a static function to sign up a user
 userSchema.statics.signup = async function(email, password) {
-    const user = await this.create({email, password});
+    //perform validation before signing up a user
+    
+    //first check if all fields exist
+    if (!email || !password) {
+        throw Error("All fields must be filled out!");
+    }
+
+    //check if email is valid, if it isn't throw an error
+    if(!validator.isEmail(email)) {
+        throw Error("Email is invalid!");
+    }
+
+    //check if password is strong, if it isn't throw an error
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough!');
+    }
+
+    //once we have passed all the above checks we should check the database to see if the user exists
+    //we do this by seeing if we can find one document in the database that contains the same email
+    const result = await this.findOne({email});
+
+    if (result) {
+        throw Error("A user already exists with this email!");
+    }
+
+    //once we have verified that the user has entered a valid email and password, and that they are a new user we hash their password
+    const salt = await bcrypt.genSalt(11);
+    const hash = await bcrypt.hash(password, salt);
+
+    //once we have our hashed password we create the user
+    const user = await this.create({email, password: hash});
     return user;
 }
 
