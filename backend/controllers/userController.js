c65d69e8ef595ec9e12c364c0116d88a46906670
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-09-26 14:13:39 -0400

    Added jsonwebtokens to the login process, when a user signs up the api sends them a token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,12 @@
 //this file will contain all of our controller functions
 //import our User model
 const User = require ('../models/userModel.js');
+const jwt = require('jsonwebtoken');
+
+//function to create a token
+const createToken = (email, _id) => {
+    return jwt.sign({email, _id}, process.env.SECRET, {expiresIn: '1d'});
+}
 
 //login function
 const loginUser = async (req, res) => {
@@ -15,7 +21,9 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password);
         
-        res.status(200).json({ user });       
+        //if we are successful in signing up create a token and send it back to the user
+        const token = createToken(user.email, user._id);
+        res.status(200).json({ email, token });       
     } catch (error) {
         //if there is an error send the error back to the user
         res.status(400).json({error: error.message});   
