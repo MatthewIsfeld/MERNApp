1504b7b36d016e21992d4bc5399706960569cdcc
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-09-27 13:28:11 -0400

    Created simple signup form to test sending requests from the frontend

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,6 +1,48 @@
+import {useState} from 'react';
+
 const Signup = () => {
+    //create state hooks to manage the input to our forms
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const submitForSignup = async (e) => {
+        //prevent the default form submit action of refreshing the page
+        e.preventDefault();
+
+        //get the response from signing up, this should be the user's email and their token
+        const response = await fetch('/app/user/signup', {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({email, password})
+        });
+
+        //convert the response to json
+        const jsonResponse = await response.json();
+
+        if (response.ok) {
+            //for now print our response to the console
+            console.log(jsonResponse);
+        }
+    }
+
     return (
-        <div>Signup Page</div>
+        <form className="signup" onSubmit={submitForSignup}>
+            <label>Email:</label>
+            <input
+            type="email"
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}>
+            </input>
+
+            <label>Password:</label>
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}>
+            </input>
+
+            <button>Sign Up</button>
+        </form>
     )
 }
 
