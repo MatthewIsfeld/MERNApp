7a546c36579eb15bb73c9e5041be84ccacac506e
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-09-27 14:06:10 -0400

    Added an error div to display errors in signup, added some state to check if the frontend is processing a response so the user does not send multiple responses

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -3,14 +3,20 @@ import {UseUserContext} from '../hooks/useUserContext.js';
 
 const Signup = () => {
     //create state hooks to manage the input to our forms
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const {dispatch} = UseUserContext();
+    //some state to check for errors, and change the form depending on whether or not we are loading information
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const submitForSignup = async (e) => {
         //prevent the default form submit action of refreshing the page
         e.preventDefault();
 
+        //we are now loading so we setIsLoading to true
+        setIsLoading(true);
+        setError(null);
         //get the response from signing up, this should be the user's email and their token
         const response = await fetch('/app/user/signup', {
             method: "POST",
@@ -27,6 +33,10 @@ const Signup = () => {
 
             //set the global context to be logged in
             dispatch({type: "LOGIN", payload: jsonResponse});
+            setIsLoading(false);
+        } else {
+            setError(jsonResponse.error);
+            setIsLoading(false);
         }
     }
 
@@ -46,7 +56,8 @@ const Signup = () => {
             onChange={(e) => {setPassword(e.target.value)}}>
             </input>
 
-            <button>Sign Up</button>
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
