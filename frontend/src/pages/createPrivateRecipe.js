264df6c5f9825951338f0fc2701fe15018b1be2f
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-10-03 16:32:16 -0400

    Added some comments to the create a new recipe page

--- frontend/src/pages/createPrivateRecipe.js
+++ frontend/src/pages/createPrivateRecipe.js
@@ -6,24 +6,29 @@ const CreatePrivateRecipe = () => {
     const [title, setTitle] = useState('');
     const [instructions, setInstructions] = useState('');
     const [meal, setMeal] = useState('any');
+    //set our ingredients to a list of javascript objects containing the name of each ingredient and the amount in grams
     const [ingredients, setIngredients] = useState([{name: '', amount: 0}]);
 
+    //This function will add more ingredient input fields at the user's request, it does this by updating the list of ingredients, which causes the page to re-run the .map in the jsx
     const addIngredientInput = () => {
         setIngredients([...ingredients, {name: "", amount: 0}]);
     }
 
+    //remove an ingredient in a similar way to how we add it
     const removeIngredientInput = (index) => {
         const tempList = [...ingredients];
         tempList.splice(index, 1);
         setIngredients(tempList);
     }
 
+    //if we change the name of a particular ingredient we take the index of where that ingredient is in the list and change the value, to codify this we setIngredients
     const handleNameChange = (e, index) => {
         const tempList = [...ingredients];
         tempList[index].name = e.target.value;
         setIngredients(tempList);
     }
 
+    //we change amount in the same fashion as name
     const handleAmountChange = (e, index) => {
         const tempList = [...ingredients];
         tempList[index].amount = e.target.value;
