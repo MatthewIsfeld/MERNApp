3c9e78e5a231ae7c7e50109536a3a13ab40ad07b
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-10-04 15:31:18 -0400

    Implemented delete recipe feature

--- frontend/src/pages/detailedRecipeDisplay.js
+++ frontend/src/pages/detailedRecipeDisplay.js
@@ -1,10 +1,41 @@
 import {useLocation} from 'react-router-dom';
 import {UseUserContext} from '../hooks/useUserContext.js';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const DetailedRecipeDisplay = () => {
     const location = useLocation();
     const {user} = UseUserContext();
     const {recipe} = location.state;
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        //check to see if we are logged in
+        if (!user) {
+            navigate('/');
+            return
+        }
+        setError(null);
+        //if we are logged in make a fetch request to delete
+        const response = await fetch(`/app/privateRecipes/${recipe._id}`, {
+            method: "DELETE",
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
+        });
+
+        const jsonResponse = await response.json();
+
+        if(response.ok) {
+            setError(null);
+            navigate('/')
+            return;
+        } else {
+            setError(jsonResponse.error);
+            return;
+        }
+    }
 
     return (
         <div className="detailed-recipe">
@@ -18,6 +49,9 @@ const DetailedRecipeDisplay = () => {
                     </div>
                 ))}
             </div>
+
+            <button onClick={handleDelete}>Delete Recipe</button>
+            {error && <div className="error">{error}</div>}
         </div>
     )
 }
