ec531089a6f4144ede7135c3860f015c2d87b3e5
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2022-11-11 13:38:17 -0500

    Recipes now display the date they were created on in MM/DD/YYYY format

--- frontend/src/components/simpleRecipeDisplay.js
+++ frontend/src/components/simpleRecipeDisplay.js
@@ -1,11 +1,13 @@
 import {Link} from 'react-router-dom';
 
 const SimpleRecipeDisplay = ({ recipe }) => {
+    const date = new Date(recipe.createdAt);
+
     return (
         <div className="simple-recipe">
             <h4>{recipe.title}</h4>
             <p>Meal: {recipe.meal}</p>
-            <p>{recipe.createdAt}</p>
+            <p>{`${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`}</p>
             <Link to="/recipe/view" state={{recipe}}>View</Link>
         </div>
     )
